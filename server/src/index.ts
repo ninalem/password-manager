54371d75aaadaa7cca36b64bde6606a6aee2c507
salsifitewn <salsifite@gmail.com>
2020-12-23 23:22:33 +0100

    build(server): Update ts compiler && moving app files to src folder
    Move app files to ./src
    
    References:
    - https://github.com/Microsoft/TypeScript-Node-Starter#configuring-typescript-compilation
    - https://www.typescriptlang.org/tsconfig

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { getConnection } from "./db/mongo-client";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+getConnection()
+  .then(async () => {
+    app.listen(port, () =>
+      console.log(`passManager app listening at http://localhost:${port}`)
+    );
+  })
+  .catch((error) => console.error(error));
