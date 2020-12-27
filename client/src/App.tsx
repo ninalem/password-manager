84d9bb95da55d951841c67e4556aee6114d21646
Stanislas Ormières <stan@stormier.ninja>
2020-12-27 03:10:49 +0100

    :sparkles: First components in React

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import AppHeader from './components/AppHeader'
+import LoginForm from './components/LoginForm'
 import './App.css';
+
+const loggedIn = false
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+      { !loggedIn && <LoginForm /> }
     </div>
   );
 }
---
+++ client/src/components/AppHeader.tsx
@@ -0,0 +1,24 @@
+import LogoutButton from './LogoutButton'
+
+const loggedIn = false
+
+function AppHeader () {
+    return (
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        {loggedIn && <LogoutButton />}
+      </header>
+    )
+}
+
+export default AppHeader
